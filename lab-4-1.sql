36f43bd362ff624b7985dead55674ff1f2e69313
VenkataSudhakar(VESU) Rayaprolu <vesu.rayaprolu@kellogg.northwestern.edu>
2022-01-16 05:09:03 +0000

    Lab-4-1 completed

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,4 @@
 -- Expected result:
 -- 2935
 
-
+SELECT SUM(hits),first_name, last_name FROM stats INNER JOIN players ON stats.player_id = players.id WHERE first_name = 'Barry' AND last_name = 'Bonds'
